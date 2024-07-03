50f0fb1337a8e728a00f59efcf8e5def9f268fe5
root <root@vmi1492008.contaboserver.net>
2024-07-03 09:40:53 +0200

    Remove duplicate files

--- protracker/authentication1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, login, test_token, logout
-
-urlpatterns = [
-    path('register', register),
-    path('login', login),
-    path('test-token', test_token),
-    path('logout', logout),
-]
